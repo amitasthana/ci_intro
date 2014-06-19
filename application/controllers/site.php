d05fec82c27a2314a2271afc133e84f83758e0fc
Amit Asthana <amitasthana.777@gmail.com>
2014-06-19 22:41:44 +0530

    this is the dams message

---
+++ application/controllers/site.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+
+	public function index(){
+
+		echo "hi Internet: ";
+
+		$this->hello(); 
+
+	}
+
+	public function hello(){
+		echo " something else";
+	}
+
+	
+}
+
